083b3e7842eeef69f078eee6556a6caecc381196
Gustavo França <84541061+Gustafranca@users.noreply.github.com>
2021-05-28 09:23:08 -0300

    sasa

---
+++ site exemplo/problema6.py
@@ -0,0 +1,6 @@
+a = int(input('Digite o primeiro inteiro: '))
+b = int(input('Digite o segundo inteiro: '))
+
+c = a % b 
+
+print(f'Resto: {c}')
---
+++ site exemplo/problema8.py
@@ -0,0 +1,5 @@
+milhas = float(input('Digite a distância dem milhas: '))
+
+km = milhas * 1.61
+
+print(f'Distância em km: {km}')
